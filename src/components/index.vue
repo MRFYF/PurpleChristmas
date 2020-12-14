14df5783d60072a0a3c33dbfd5a90499f9744aac
xzh <408796248@qq.com>
2020-12-14 17:04:53 +0800

    xzj

---
+++ src/components/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>TES123T</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+body {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+</style>
